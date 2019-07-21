5edc1b1fefc306ac25bf24d7b4928d19511d84a9
Elias Kosunen <elias.kosunen@gmail.com>
2019-07-21 22:25:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# scnlib-spirit-benchmark
+
+Benchmarking [scnlib](https://github.com/eliaskosunen/scnlib) against Boost.Spirit x3,
+as prompted by [issue #9](https://github.com/eliaskosunen/scnlib/issue/9).
+
+## Results
+
+### spirit-oriented benchmark
+
+Benchmark | CPU time
+:-------- | -------:
+spirit    | 7258
+scnlib    | 12426
---
+++ bench.cpp
@@ -0,0 +1,81 @@
+#include <benchmark/benchmark.h>
+#include <scn/scn.h>
+#include <boost/spirit/home/x3.hpp>
+
+#include <algorithm>
+#include <cmath>
+#include <random>
+#include <sstream>
+#include <string>
+#include <vector>
+
+template <typename Float>
+std::string generate_data(size_t n)
+{
+    std::default_random_engine rng(std::random_device{}());
+    std::uniform_int_distribution<int> int_dist(-8, 16);
+    std::uniform_real_distribution<Float> float_dist(Float(0.0), Float(1.0));
+
+    std::ostringstream oss;
+    for (size_t i = 0; i < n; ++i) {
+        auto f = float_dist(rng);
+        auto exp = int_dist(rng);
+        f = std::scalbn(f, exp);
+        oss << f << ", ";
+    }
+    oss << float_dist(rng);
+    return oss.str();
+}
+
+static void issue_spirit(benchmark::State& state)
+{
+    using namespace boost::spirit;
+
+    for (auto _ : state) {
+        auto data = generate_data<double>(4);
+
+        std::vector<double> read;
+        bool r = x3::phrase_parse(data.begin(), data.end(),
+                                  x3::double_ >> *(',' >> x3::double_),
+                                  x3::ascii::space, read);
+        if (!r) {
+            state.SkipWithError("");
+            break;
+        }
+    }
+}
+BENCHMARK(issue_spirit);
+
+static void issue_scn(benchmark::State& state)
+{
+    using namespace boost::spirit;
+
+    for (auto _ : state) {
+        auto data = generate_data<double>(4);
+
+        std::vector<double> read;
+        auto stream = scn::make_stream(data);
+        while (true) {
+            std::string str;
+            auto err = scn::getline(stream, str, ',');
+            if (!err) {
+                if (err == scn::error::end_of_stream) {
+                    break;
+                }
+                state.SkipWithError("");
+                break;
+            }
+
+            auto strstream = scn::make_stream(str);
+            auto tmp = scn::get_value<double>(strstream);
+            if (!tmp) {
+                state.SkipWithError("");
+                break;
+            }
+            read.push_back(tmp.value());
+        }
+    }
+}
+BENCHMARK(issue_scn);
+
+BENCHMARK_MAIN();
